df646e745b48e4e893cbc8feea572543747476af
TomasRingefelt <tomas.ringefelt@gmail.com>
2014-06-10 16:55:02 +0200

    Inital commit

---
+++ Assets/Scripts/GameController/LastPlayerSighting.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LastPlayerSighting : MonoBehaviour
+{
+		public Vector3 position = new Vector3 (1000f, 1000f, 1000f);
+		public Vector3 resetPosition = new Vector3 (1000f, 1000f, 1000f);
+		public float lightHighIntensity = 0.25f;
+		public float lightLowIntesity = 0f;
+
+		public float fadeSpeed = 7f;
+		public float musicFadeSpeed = 1f;
+
+		private AlarmLight alarm;
+		private Light mainLight;
+		private AudioSource panicAudio;
+		private AudioSource[] sirens;
+		private bool playingAlarm = false;
+		 
+
+		void Awake ()
+		{
+				alarm = GameObject.FindGameObjectWithTag (Tags.alarm).GetComponent<AlarmLight> ();
+				mainLight = GameObject.FindGameObjectWithTag (Tags.mainLight).light;
+				panicAudio = transform.Find ("secondaryMusic").audio;
+				GameObject[] sirenGameObjects = GameObject.FindGameObjectsWithTag (Tags.siren);
+				sirens = new AudioSource[sirenGameObjects.Length];
+
+				for (int i = 0; i < sirenGameObjects.Length; i++) {
+						sirens [i] = sirenGameObjects [i].audio;
+				}
+		}
+
+		void Update ()
+		{
+				SwithAlarms ();
+				MusicFading ();
+		}
+
+		void SwithAlarms ()
+		{
+				alarm.alarmOn = (position != resetPosition);
+				
+				float newIntensity;
+
+				if (alarm.alarmOn) {
+						newIntensity = lightLowIntesity;
+				} else {
+						newIntensity = lightHighIntensity;
+				}
+				mainLight.intensity = Mathf.Lerp (mainLight.intensity, newIntensity, fadeSpeed * Time.deltaTime);
+
+				if (alarm.alarmOn) {
+						for (int i = 0; i < sirens.Length; i++) {
+								if (!playingAlarm) {
+										playingAlarm = true;
+										sirens [i].Play ();
+								}
+						}
+				} else {
+						for (int i = 0; i < sirens.Length; i++) {
+								if (playingAlarm) {
+										playingAlarm = false;
+										sirens [i].Stop ();
+								}
+						}
+				}
+		}
+
+		void MusicFading ()
+		{
+				if (position != resetPosition) {
+						audio.volume = Mathf.Lerp (audio.volume, 0f, musicFadeSpeed * Time.deltaTime);
+						panicAudio.volume = Mathf.Lerp (panicAudio.volume, 0.8f, musicFadeSpeed * Time.deltaTime);
+
+				} else {
+						audio.volume = Mathf.Lerp (audio.volume, 0.8f, musicFadeSpeed * Time.deltaTime);
+						panicAudio.volume = Mathf.Lerp (panicAudio.volume, 0f, musicFadeSpeed * Time.deltaTime);
+
+				}
+		}
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+		public float playerHealth = 100f;
+		public float resetAfterDeathTime = 5f;
+		public AudioClip deathClip; 
+
+		private Animator anim;
+		private PlayerMovement playerMovement;
+		private HashIDs hash;
+		private SceneFadeInAndOut sceneFadeInAndOut;
+		private LastPlayerSighting lastPlayerSighting;
+		private float timer;
+		private bool playerDead = false;
+
+
+		void Awake ()
+		{
+				hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+				if ((anim = GetComponent<Animator> ()) == null) {
+						Debug.LogWarning ("Warning! Animator component not found", this);
+				}
+				if ((playerMovement = GetComponent<PlayerMovement> ()) == null) {
+						Debug.LogWarning ("playerMovement can't be found in gameController! playerMovement component is null!", this);
+				}
+				
+				if ((sceneFadeInAndOut = GameObject.FindGameObjectWithTag (Tags.fader).GetComponent<SceneFadeInAndOut> ()) == null) {
+						Debug.LogWarning ("Warning! SceneFader component not found", this);
+				}
+				if ((lastPlayerSighting = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<LastPlayerSighting> ()) == null) {
+						Debug.LogWarning ("Warning! LastPlayerSighting component not found", this);
+				}
+		}
+
+		void Update ()
+		{
+				if (playerHealth <= 0f) {
+						if (!playerDead) {
+								PlayerDying ();
+						} else {
+								PlayerDead ();
+								LevelReset ();
+						}
+				}
+		}
+
+
+		void PlayerDying ()
+		{
+				playerDead = true;
+				anim.SetBool (hash.deadBool, true);
+				AudioSource.PlayClipAtPoint (deathClip, transform.position);
+		}
+
+		void PlayerDead ()
+		{
+				if (anim.GetCurrentAnimatorStateInfo (0).nameHash == hash.dyingState) {
+						anim.SetBool (hash.deadBool, false);
+				}
+				
+				anim.SetFloat (hash.speedFloat, 0f);
+				playerMovement.enabled = false;
+				lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+				audio.Stop ();
+		}
+
+		void LevelReset ()
+		{
+				timer += Time.deltaTime;
+				if (timer >= resetAfterDeathTime) {
+						sceneFadeInAndOut.EndScene ();
+				} 
+		}
+
+		public void TakeDamage (float amout)
+		{
+				if (amout >= 0f) {
+						playerHealth -= amout;
+				}
+		}
+}
